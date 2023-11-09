769b6907db0b7c0260f35626ee6869ad8baa4f05
ArjunDahal99 <dahalarjun409@gmail.com>
2023-11-09 21:27:42 +0545

    ok

--- controllers/generateContoller.js
+++ controllers/generateContoller.js
@@ -1,9 +1,10 @@
 import Replicate from "replicate";
+import got from 'got' // if you don't have "got" - install it with "npm install got"
 import { v2 as cloudinary } from "cloudinary";
 import db from "../utils/db-server.js";
 
 const replicate = new Replicate({
-  auth: "r8_IIUXxJmBaYiGyVIfiR94pxlJ1BQlO4q4SuTt6",
+  auth: "r8_W6J9Mh7LCdUqR00xue5tRdyaNkbnsKJ0msq3q",
 });
 
 cloudinary.config({
@@ -13,9 +14,18 @@ cloudinary.config({
   secure: true,
 });
 
-export const generateImage = async (req, res) => {
-  const { prompt, height, width, engineModel, email } = await req.body;
 
+
+const apiKey = 'acc_e00fb21ec8b4b69';
+const apiSecret = '2bc6a4f3eaec6383a9b966d6a5f8ce0a';
+
+
+
+
+
+export const generateImage = async (req, res) =>
+{
+  const { prompt, height, width, engineModel, email, isFeatured } = await req.body;
   const userFromdb = await db.user.findFirst({
     where: {
       email,
@@ -27,7 +37,14 @@ export const generateImage = async (req, res) => {
       message: "Sign in First",
     });
 
-  try {
+
+
+
+  //generate image from replicate
+  try
+  {
+    var tagNames;
+    var colorData;
     let output = await replicate.run(engineModel, {
       input: {
         prompt,
@@ -35,8 +52,58 @@ export const generateImage = async (req, res) => {
         width,
       },
     });
-    console.log(output[0]);
-    await cloudinary.uploader.upload(output[0]).then(async (result) => {
+
+
+    // get tags from imagga
+
+    const imageUrl = output[0];
+    const url = 'https://api.imagga.com/v2/tags?image_url=' + encodeURIComponent(imageUrl);
+
+    (async () =>
+    {
+      try
+      {
+        const response = await got(url, { username: apiKey, password: apiSecret });
+
+        const data = await JSON.parse(response.body);
+        const tagsWithHighConfidence = data.result.tags
+          .filter((tag) => tag.confidence > 20)
+          .map((tag) => tag.tag.en);
+        tagNames = await tagsWithHighConfidence.map((tagName) => tagName);
+
+
+      } catch (error)
+      {
+        console.log(error);
+      }
+    })();
+    //get color data from the image
+    const urlforColor = 'https://api.imagga.com/v2/colors?image_url=' + encodeURIComponent(imageUrl);
+    (async () =>
+    {
+      try
+      {
+        const response = await got(urlforColor, { username: apiKey, password: apiSecret });
+        const data = await JSON.parse(response.body);
+        const newData = await data.result.colors.background_colors;
+        colorData = await newData.map((i) => ({
+          color: i.closest_palette_color,
+          colorCode: i.html_code,
+          percentage: i.percent,
+        }));
+
+      } catch (error)
+      {
+        console.log(error);
+      }
+    })();
+
+
+    //finally upload to cloud and the cloudurl is saved to the database
+
+    await cloudinary.uploader.upload(output[0]).then(async (result) =>
+    {
+
       const image = await db.image.create({
         data: {
           height,
@@ -44,8 +111,15 @@ export const generateImage = async (req, res) => {
           url: result.url,
           userId: userFromdb.id,
           prompt,
+          isFeatured,
+          tags: tagNames.map((t) => t),
+          BackgroundColor: {
+            create: colorData,
+          },
         },
       });
+
+      //reduce the token from the user
       const user = await db.user.update({
         where: { id: userFromdb.id },
         data: {
@@ -55,7 +129,8 @@ export const generateImage = async (req, res) => {
 
       res.json(image);
     });
-  } catch (error) {
+  } catch (error)
+  {
     console.log(error);
   }
 };
