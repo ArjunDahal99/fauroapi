11aa7b2cc56e641cf3cbdc86f5485f932f716e52
ArjunDahal99 <dahalarjun409@gmail.com>
2023-11-15 18:50:25 +0545

    db

--- utils/db-server.js
+++ utils/db-server.js
@@ -2,7 +2,9 @@
 import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis;
 
-globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient();
+globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL } }
+})
 
 
 const db = globalForPrisma.prisma
